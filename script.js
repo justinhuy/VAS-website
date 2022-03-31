c917f4e68b84944fe60f943357a7ae2eff2bd33f
JustinHuy <trannhathuy1305@gmail.com>
2022-03-31 23:51:31 +0700

    J

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ $(document).ready(function(){
         $('.navbar').toggleClass('nav-toggle');
     });
 
+    $('#login').click(function(){
+        $('.login-form').addClass('popup');
+    });
+
+    $('.login-form form .fa-times').click(function(){
+        $('.login-form').removeClass('popup');
+    });
+
     $(window).on('load scroll',function(){
 
 
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+        $('.login-form').removeClass('popup');
     })
 });
